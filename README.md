8cb958583ddec26926fb3781d3e850fd9842076f
jurrehageman <jurrehageman@gmail.com>
2020-11-20 15:07:49 +0100

    added argparse arguments

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Sort BB files
 
 - Sorts files downloaded from a BB test in folders.
+- Converts heic files in jpg files.
 - Generates bundled pdf document for all jpg pictures per student.
 
 ## Getting Started
@@ -30,24 +31,34 @@ pip3 install pyheif
 
 This module will sort files in folders and will generate bundled pdf files.
 
-- First replace the zip file in the folder `place_zip_here` with your downloaded BB zip file.
-- Then run `sort2folder.py`
-- Copy your data
-- Run `reset_folder.py` to reset.
+Mac/Linux:
+First make the file executable:
 
+```
+chmod u+x sort2folder.py
+```
+Then run the script
 Example Mac/Linux:
 ```
-python3 sort2folder.py
+./sort2folder.py -pcr test.zip outdirname
 ```
 On windows:
 ```
-python sort2folder.py
+python sort2folder.py -pcr test.zip outdirname
 ```
+Warning: The pyheif module will not work on Windows so we do not recommend to use Windows. 
 
 ### Arguments:
 
 Required arguments:
-- no command-line arguments used
+- `zip_file`: path to the zip file
+- `output_folder`: path to the output folder
+
+Optional arguments:
+- `--pdf or -p`: bundles all jpg files per student to pdf 
+- `--checksum or -c`: creates csv file with md5 checksums for all files. Use Excel conditional formatting to find duplicates.
+- `--randomize or -r`: adds random numbers to student numbers to randomize folder/file sequence.
+- `--help or -h`: shows help on arguments.
 
 
 ## Authors
--- sort2folder.py
+++ sort2folder.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import os
 import sys
 import shutil
@@ -6,26 +7,38 @@ from PIL import Image
 import platform
 import warnings
 import hashlib
+import argparse
+import random
 #PLATFORM
 OS_TYPE = platform.platform()
 if not OS_TYPE.startswith("Windows"):
     import pyheif
 
-
 #SETTINGS
-pdf_export = True
 MAX_PATH = 260
-
-#FILE LOCATIONS
-zip_in_loc = os.path.join(".", "place_zip_here")
-zip_out_loc = os.path.join(".", "unzipped_files")
-file_destination = os.path.join(".", "your_sorted_files")
-pdf_destination = os.path.join(".", "your_pdf_files")
-log_destination = os.path.join(".", "your_log_file")
-md5_destination = os.path.join(".", "your_md5_file")
 
 #ERROR FLAGS
 jpg_to_pdf_err = False
+
+def get_comm_args():
+    """
+    Reads command line arguments
+    """
+    parser = argparse.ArgumentParser(
+        description="Sort files downloaded from Blackboard")
+    parser.add_argument("zip_file",
+                        help="the path to the Zip File downloaded from Blackboard")
+    parser.add_argument("output_folder",
+                        help="the path to the Folder with the output")
+    parser.add_argument('-p', '--pdf', action='store_true',
+                        help="exports to pdf")
+    parser.add_argument('-c', '--checksum', action='store_true',
+                        help="generates md5 checksums")
+    parser.add_argument('-r', '--randomize', action='store_true',
+                        help="adds random numbers to student numbers to randomize folder/file sequence")
+    args = parser.parse_args()
+    return args
+
 
 def is_jpg(filename):
     try:
@@ -34,67 +47,69 @@ def is_jpg(filename):
     except IOError:
         return False
 
-def make_folders(folders):
-    for folder in folders:
-        if not os.path.exists(folder):
-            os.makedirs(folder)
 
-def unpack_zip(zip_in_loc, zip_out_loc):
-    all_files = os.listdir(zip_in_loc)
-    #For Mac-users (.DS_Store hidden file is created)
-    for i in all_files:
-        if i.startswith(".DS"):
-            all_files.remove(i)
-    if len(all_files) < 1:
-        print("No files in folder:", zip_in_loc)
-        sys.exit()
-    elif len(all_files) > 1:
-        print("Only one file is allowed. Found multiple files in folder:", zip_in_loc)
-        sys.exit()
-    file_name, file_extension = os.path.splitext(all_files[0])
+def make_folder(out_folder):
+    if not os.path.exists(out_folder):
+        os.makedirs(out_folder)
+
+
+def windows_warnings():
+    warnings.warn("HEIC to jpg conversion will not work on Windows")
+    len_path_zip = len(os.getcwd())
+    if len_path_zip > (MAX_PATH - 150):
+        warnings.warn("You are approaching MAX_PATH. If unzipping fails, run this script from de Desktop.")
+
+
+def unpack_zip(zip_in_loc, out_folder):
+    file_name, file_extension = os.path.splitext(zip_in_loc)
+    unzipped_files = os.path.join(out_folder, "unzipped_files")
+    make_folder(unzipped_files)
     if not file_extension == ".zip":
         print(file_name + file_extension, "is not a zip file. Only zip files are allowed.")
         sys.exit()
     print("Extracting zip file...")
-    full_path = os.path.join(zip_in_loc, all_files[0])
-    # Windows MAX_PATH may be limited to 260 in registry
-    if OS_TYPE.startswith("Windows"):
-        warnings.warn("HEIC to jpg conversion will not work on Windows")
-        len_path_zip = len(os.getcwd() + full_path[1:])
-        if len_path_zip > (MAX_PATH - len(all_files[0])):
-            warnings.warn("You are approaching Windows MAX_PATH which is limited to 260 characters. If unzipping fails, run this script from de Desktop.")
-    with ZipFile(full_path, 'r') as zipObj:
-        zipObj.extractall(zip_out_loc)
+    with ZipFile(zip_in_loc, 'r') as zipObj:
+        zipObj.extractall(unzipped_files)
     print("Zip file extracted")
 
 
-def read_files(source, destination):
+def sort_files(out_folder, randomize):
+    source = os.path.join(out_folder, "unzipped_files")
     all_files = os.listdir(source)
+    destination = os.path.join(out_folder, "sorted_files")
     students = []
     for i in all_files:
+        salt = ""
         #For Mac-users (__MACOSX folder is created)
         if i.startswith("__MAC"):
             continue
-        whole_path = os.path.join(source, i)
         student = i.split("_")[1]
-        out_path = os.path.join(destination, student)
-        if not os.path.exists(out_path):
-            os.mkdir(out_path)
-        out_path_file = os.path.join(out_path, i)
         if not student in students:
-            print("Sorting files to", student)
+            if randomize:
+                salt = str(random.randint(100, 999)) + "_"
+            out_path = os.path.join(destination, salt + student)
+            make_folder(out_path)
+            whole_path = os.path.join(source, i)
+            out_path_file = os.path.join(out_path, i)
+            shutil.copyfile(whole_path, out_path_file)
             students.append(student)
-        shutil.copyfile(whole_path, out_path_file)
+        else:
+            whole_path = os.path.join(source, i)
+            out_path_file = os.path.join(out_path, i)
+            shutil.copyfile(whole_path, out_path_file)
 
 
-def export_to_pdf(jpg_destination, pdf_destination):
+def export_to_pdf(out_folder):
+    pdf_destination = os.path.join(out_folder, "pdf_files")
+    sorted_files = os.path.join(out_folder, "sorted_files")
+    make_folder(pdf_destination)
     global jpg_to_pdf_err
     first_image_obj = None
     jpg_ext = ".jpg .JPG .jpeg .JPEG".split()
     print("Exporting to pdf...")
-    all_folders = os.listdir(jpg_destination)
+    all_folders = os.listdir(sorted_files)
     for subfolder in all_folders:
-        whole_path = os.path.join(jpg_destination, subfolder)
+        whole_path = os.path.join(sorted_files, subfolder)
         all_files = os.listdir(whole_path)
         first_image_obj = None
         image_list = []
@@ -120,21 +135,26 @@ def export_to_pdf(jpg_destination, pdf_destination):
             first_image_obj.save(pdf_filename, "PDF", resolution=100.0, save_all=True, append_images=image_list)
 
 
-def write_log(zip_out_loc, log_destination):
-    full_path = os.path.join(log_destination, "log_file.txt")
+def write_log(out_folder):
+    print("writing log...")
+    log_path = os.path.join(out_folder, "log")
+    make_folder(log_path)
+    log_file = os.path.join(log_path, "log_file.txt")
+    zip_location = os.path.join(out_folder, "unzipped_files")
     pic_to_skip = ".jpg .jpeg .JPG .JPEG".split()
     others_to_skip = [".txt"]
-    with open(full_path, "w") as f:
+    with open(log_file, "w") as f:
         f.write("Files with other extension then jpg or txt\n")
         f.write("or files with .jpg extension but fail jpg integrity test:\n\n")
-        all_files = os.listdir(zip_out_loc)
+        all_files = os.listdir(zip_location)
         for i in all_files:
             file_name, file_extension = os.path.splitext(i)
             if not file_extension in pic_to_skip and file_extension not in others_to_skip:
                 f.write(i + "\n")
-            elif not is_jpg(os.path.join(zip_out_loc, i)) and file_extension not in others_to_skip:
+            elif not is_jpg(os.path.join(zip_location, i)) and file_extension not in others_to_skip:
                 print(i)
                 f.write(i + "\n")
+
 
 def md5(fname):
     hash_md5 = hashlib.md5()
@@ -144,13 +164,15 @@ def md5(fname):
     return hash_md5.hexdigest()
 
 
-def write_md5_checksum(zip_out_loc, log_destination):
-    full_path = os.path.join(log_destination, "file_hashes.csv")
-    with open(full_path, "w") as csv:
+def write_md5_checksum(out_folder):
+    print("Creating md5 hashes...")
+    md5_file = os.path.join(out_folder, "log", "file_hashes.csv")
+    unzipped_files = os.path.join(out_folder, "unzipped_files")
+    with open(md5_file, "w") as csv:
         csv.write("Filepath; MD5 checksum\n\n")
-        all_files = os.listdir(zip_out_loc)
+        all_files = os.listdir(unzipped_files)
         for i in all_files:
-            file_path = os.path.join(zip_out_loc, i)
+            file_path = os.path.join(unzipped_files, i)
             file_hash = md5(file_path)
             csv.write("{};{}\n".format(i, file_hash))  # Get the hexadecimal digest of the hash
 
@@ -172,24 +194,22 @@ def export_heic_to_jpg(file_destination):
                 out_path_new = os.path.join(whole_path, new_name)
                 image.save(out_path_new + ".jpg", "JPEG")
 
+
 def main():
-    folders = [zip_out_loc, file_destination, log_destination, md5_destination]
-    if pdf_export:
-        folders.append(pdf_destination,)
-    make_folders(folders)
-    unpack_zip(zip_in_loc, zip_out_loc)
-    read_files(zip_out_loc, file_destination)
-    if OS_TYPE.startswith("Linux") or OS_TYPE.startswith("Darwin") or OS_TYPE.startswith("macOS"):
-        export_heic_to_jpg(file_destination)
-    if pdf_export:
-        export_to_pdf(file_destination, pdf_destination)
-    write_log(zip_out_loc, log_destination)
-    write_md5_checksum(zip_out_loc, md5_destination)
-    print("Files are sorted to", file_destination)
-    if pdf_export:
-        print("PDF file is saved to", pdf_destination)
-    print("Log file saved to", log_destination)
-    print("MD5 checksum saved to", md5_destination)
+    # if OS_TYPE.startswith("Windows"):
+    #     windows_warnings()
+    args = get_comm_args()
+    zip_file = args.zip_file
+    out_folder = args.output_folder
+    unpack_zip(zip_file, out_folder)
+    sort_files(out_folder, args.randomize)
+    if not OS_TYPE.startswith("Windows"):
+        export_heic_to_jpg(os.path.join(out_folder, "sorted_files"))
+    if args.pdf:
+        export_to_pdf(out_folder)
+    write_log(out_folder)
+    if args.checksum:
+        write_md5_checksum(out_folder)
     if jpg_to_pdf_err:
         print("Corrupt jpg encountered. See log file for details")
     print("Done")
